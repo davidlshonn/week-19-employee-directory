e4ecf94a9b8fbdce98b622706918fd37e26a5215
David Shonn <davidlshonn@icloud.com>
2021-03-24 20:51:54 +0000

    Added some components and pseudo code

---
+++ src/components/TableArea.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class TableArea extends React.Component {
+    //Declare state to hold employyes and filtered employees
+
+
+    //handleSearch function
+
+    //handleSort function
+
+    //Function being called which returns the comployees from the API within a componentDidMount
+
+    //render two other components:
+        //SearchBar - takes a handleSearch function
+        //Table - takes the employees and a function called handleSort
+        
+}
