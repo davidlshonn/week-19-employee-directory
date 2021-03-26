8f438a4fd330e0efe2ee55d0dc8aec453aed4a7a
David Shonn <davidlshonn@icloud.com>
2021-03-26 18:36:43 +0000

    Added code for getting employees and setting in state

--- src/components/TableArea.js
+++ src/components/TableArea.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import API from '../utils/API'
 
 export default class TableArea extends React.Component {
     //Declare state to hold employyes and filtered employees
-
+    state = {
+            employees: [{}],
+            filteredUsers: [{}]
+    }
 
     //handleSearch function
 
     //handleSort function
 
     //Function being called which returns the comployees from the API within a componentDidMount
-
+    componentDidMount(){
+        API.getEmps().then(response => {
+            this.setState({
+                employees: response.data.result,
+                filteredUsers: response.data.results,
+            })
+        })
+    }
     //render two other components:
         //SearchBar - takes a handleSearch function
         //Table - takes the employees and a function called handleSort
+        render(){
+            return (
+                <h4>This is where the table etc will be</h4>
+            )
+        }
         
 }
